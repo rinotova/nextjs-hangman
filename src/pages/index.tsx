117bb432ac6a0798305464d6e1b91ba59cd217f4
Rino Tovar <>
2023-04-27 15:43:59 +0200

    Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { type NextPage } from "next";
+import { signIn, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <h1>Hello world</h1>;
+  const { data: session } = useSession();
+  const signInHandler = () => {
+    void signIn("google");
+  };
+  return (
+    <div>
+      <h1>Hello world</h1>
+      {!session && (
+        <button
+          onClick={signInHandler}
+          className="mt-8 rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-black/20"
+        >
+          Sign in with Google
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
